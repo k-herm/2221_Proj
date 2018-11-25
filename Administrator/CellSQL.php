5c9160e954060d79ae8d24927b17fa0adbb4bcfb
mslavnic <32505020+mslavnic@users.noreply.github.com>
2018-11-24 23:18:13 -0800

    Update CellSQL.php

--- Administrator/CellSQL.php
+++ Administrator/CellSQL.php
@@ -3,3 +3,27 @@
 
     echo a table with <a> links on cell id
 -->
+<?php
+	require_once '../helperFunctions.php';
+	$conn = connectToDatabase();
+	//echo all inmates with links
+	$query = "SELECT * FROM cell";
+	$result = $conn->query($query);
+
+
+	//echo all inmates with links
+	if($result->num_rows > 0){
+		echo "<br> ID". "\t Max_Occupants". "\t Location <br>"; 
+		while($row = $result->fetch_assoc()){
+			extract($row);
+			//$link = $row["Cell_ID"];
+			//echo "<a href=INSERT APPROPRIATE LINK HERE>$link</a>";
+			echo "\t". $row["Cell_ID"]. "\t". $row["Max_Occupants"]. "\t". $row["Location"]. "<br>";
+		}
+	}
+	else{
+		echo "0 results!";
+	}
+	clearConnection($conn);
+
+?>
