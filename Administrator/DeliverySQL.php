a13e95d5f567b5cf88547b9b150eaa8676e2e204
donovanwoods <donovan.t.woods@gmail.com>
2018-11-25 22:52:22 -0700

    DeliverySQL Sort By Freq

--- Administrator/DeliverySQL.php
+++ Administrator/DeliverySQL.php
@@ -10,6 +10,31 @@
 	require_once '../helperFunctions.php';
     $conn = connectToDatabase();
 
+if(isset($_GET['stats'])) {
+	$query = "SELECT name, company_id, frequency
+              FROM Delivery_company 
+              GROUP BY company_id
+              ORDER BY 
+                CASE frequency
+                    WHEN 'ON DEMAND' THEN 1
+                    WHEN 'WEEKLY' THEN 2
+                    WHEN 'BI-WEEKLY' THEN 3
+                    ELSE 4
+                    END";
+	$result = $conn->query($query);
+
+
+	if($result->num_rows > 0){
+		echo "<h1> Companies Based on Frequency </h1>"; 
+		echo "<table align=\"center\"border= \"1\">";
+        echo "<tr><th>Delivery Company</th><th>ID</th><th>Frequency</th>";
+		while($row = $result->fetch_assoc()){
+			echo "<tr><td>". $row["name"]. "</td><td>". $row["company_id"] . "</td><td>". $row["frequency"]. "</td></tr>";
+		}
+	}
+}
+else{
+
 	$deliveryName=$_POST['deliveryName'];
     $deliveryID=$_POST['deliveryID'];
 
@@ -28,7 +53,7 @@
 	if($result->num_rows > 0){
 		echo "<h1>Companies based on search:</h1>";
         echo "<table align=\"center\"border= \"1\">";
-        echo "<tr><th>Delivey Company</th><th>ID";
+        echo "<tr><th>Delivery Company</th><th>ID";
         // output data for each row
         while($row = $result->fetch_assoc()){
         	echo "<tr><td>" . $row["Name"] . "</td><td>". $row["Company_ID"] . "</td></tr>";
@@ -37,6 +62,7 @@
 	}	else{
 		echo "<h1>0 result found with this ID and Name</h1>";
 	}
+}
     
 	$conn->close();
 ?>
