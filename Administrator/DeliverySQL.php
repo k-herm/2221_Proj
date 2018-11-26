a996ece8a32da9aea60bb69f137c2d74f029a328
donovanwoods <donovan.t.woods@gmail.com>
2018-11-25 18:52:15 -0700

    DeliverySQL

--- Administrator/DeliverySQL.php
+++ Administrator/DeliverySQL.php
@@ -5,3 +5,38 @@
     deliveryID
 -->
 
+<?php
+
+	require_once '../helperFunctions.php';
+    $conn = connectToDatabase();
+
+	$deliveryName=$_POST['deliveryName'];
+    $deliveryID=$_POST['deliveryID'];
+
+	//Checking conn
+	if($conn->connect_error){
+		die("Connection failed: ". $conn->connecte_error);
+	}
+
+
+
+	//construct query
+	$query = "SELECT * FROM Delivery_company WHERE name = '$deliveryName' OR company_ID = '$deliveryID'";
+	$result = $conn->query($query);
+
+	//Execute query
+	if($result->num_rows > 0){
+		echo "<h1>Shipping</h1>";
+        echo "<table align=\"center\"border= \"1\">";
+        echo "<tr><th>Delivey Company</th><th>ID";
+        // output data for each row
+        while($row = $result->fetch_assoc()){
+        	echo "<tr><td>" . $row["Name"] . "</td><td>". $row["Company_ID"] . "</td></tr>";
+        }
+
+	}	else{
+		echo "<h1>0 result found with this ID and Name</h1>";
+	}
+    
+	$conn->close();
+?>
